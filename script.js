41c574cfb7aa0ec1f4cc9b915b92b909e40e0191
vitalii <dpool183@gmail.com>
2020-10-24 15:36:06 +0500

    Сделал Lazy Load для картинок

--- script.js
+++ script.js
@@ -48,4 +48,31 @@ $(document).ready(function(){
 	});
 
 
-});		
+});
+
+const images = document.querySelectorAll('img');
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+}
+
+function handleImg(myImg, observer) {
+    myImg.forEach(myImgSingle => {
+        console.log(myImgSingle.intersectionRatio);
+        if (myImgSingle.intersectionRatio > 0) {
+            loadImage(myImgSingle.target);
+        }
+    })
+}
+
+function loadImage(image) {
+    image.src = image.getAttribute('data');
+}
+
+const observer = new IntersectionObserver(handleImg, options);
+
+images.forEach(img => {
+    observer.observe(img);
+})		
